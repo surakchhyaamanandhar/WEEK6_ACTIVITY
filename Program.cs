ab9f87e060fbecb33187f7d83ffdce21736095ad
surakchhyaamanandhar <surakchhyamndr5@gmail.com>
2024-08-11 16:08:24 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Select an operation: +, -, *, /");
+        string operation = Console.ReadLine();
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operation selected.");
+                return;
+        }
+
+        Console.WriteLine("The result is: " + result);
+    }
+}
